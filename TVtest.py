988429768acab65b27276042ff3477ec76449d1e
Joash <guanzonjoashmatthew@gmail.com>
2024-05-20 19:51:11 +0800

    Added the GUI interface statements and outputs

--- TVtest.py
+++ TVtest.py
@@ -20,4 +20,11 @@ def TVtest():
     tv2.setChannel(3)
     tv2.setVolume(2)
     
-    
+    TestTV = 'TV sets'
+
+    # TEST TV
+    TestTV_layout = [
+        [sg.Text(TestTV.center(50), font=('Lovelo', 25), justification='center')],
+        [sg.Frame('', [[sg.Text(f"TV1's channel is {tv1.getChannel()} and volume level is {tv1.getVolume()}.", size=(35,1), font=('Elephant',12))]], background_color='Blue',border_width=3)],
+        [sg.Frame('', [[sg.Text(f"TV2's channel is {tv2.getChannel()} and volume level is {tv2.getVolume()}.", size=(35,1), font=('Elephant',12))]], background_color='Red',border_width=3)]
+    ]
