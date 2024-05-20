913750fcf0facdfd2e741e33730341ea964ad9bc
Joash <guanzonjoashmatthew@gmail.com>
2024-05-20 20:17:51 +0800

    Added Final GUI Settings and Statements and window interface

--- TVtest.py
+++ TVtest.py
@@ -10,21 +10,31 @@ def TVtest():
     tv1 = TV()
     tv2 = TV()
 
-    # Turns the tv1 and set its channel t0 30 and volume to 3
+    # Turns the tv1 and set its channel and volume to
     tv1.turnOn()
     tv1.setChannel(30)
     tv1.setVolume(3)
 
-    # Turns the tv2 and sets its channel to 3 and volume to 2
+    # Turns the tv2 and sets its channel and volume to 
     tv2.turnOn()
     tv2.setChannel(3)
     tv2.setVolume(2)
     
-    TestTV = 'TV sets'
+    TestTV = 'TV'
 
     # TEST TV
     TestTV_layout = [
         [sg.Text(TestTV.center(50), font=('Lovelo', 25), justification='center')],
-        [sg.Frame('', [[sg.Text(f"TV1's channel is {tv1.getChannel()} and volume level is {tv1.getVolume()}.", size=(35,1), font=('Elephant',12))]], background_color='Blue',border_width=3)],
-        [sg.Frame('', [[sg.Text(f"TV2's channel is {tv2.getChannel()} and volume level is {tv2.getVolume()}.", size=(35,1), font=('Elephant',12))]], background_color='Red',border_width=3)]
+        [sg.Frame('', [[sg.Text(f"TV1's channel is {tv1.getChannel()} and volume level is {tv1.getVolume()}.", size=(35,1), font=('Times New Roman',12))]], background_color='White',border_width=3)],
+        [sg.Frame('', [[sg.Text(f"TV2's channel is {tv2.getChannel()} and volume level is {tv2.getVolume()}.", size=(35,1), font=('Times New Roman',12))]], background_color='White',border_width=3)]
     ]
+    
+    #TestTV window
+    TestTV_window = sg.Window('Test Driver Program', TestTV_layout, size=(400, 150))
+    
+    # Read and close window
+    TestTV_window.read()
+    TestTV_window.close()
+
+if __name__ == '__main__':
+    TVtest()
